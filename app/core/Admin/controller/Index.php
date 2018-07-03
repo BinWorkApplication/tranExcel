a2f0b0e91fb0630fcd858afad4d3a4527410a361
BinWorkApplication <38447061+BinWorkApplication@users.noreply.github.com>
2018-07-03 16:56:51 +0800

    Add files via upload

---
+++ app/core/Admin/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Date: 2018/6/27
+ * Time: 19:38
+ */
+
+namespace app\core\admin;
+
+use bing\core\Controller;
+
+
+class Index extends Controller
+{
+    public function index(){
+        echo 'Hello World';
+    }
+}
